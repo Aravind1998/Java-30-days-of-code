61ced60278004a9622123429716b10ba1622eb7f
Aravind1998 <aravindsuresh98@gmail.com>
2017-02-04 12:29:52 +0530

    Update Solution.java

--- Day3/Solution.java
+++ Day3/Solution.java
@@ -3,21 +3,27 @@ import java.util.*;
 import java.text.*;
 import java.math.*;
 import java.util.regex.*;
+
 public class Solution {
-   
-   public static void main(String[] args) {
-      Scanner scan = new Scanner(System.in);
-      int n = scan.nextInt(); 
-      scan.close();
-      String ans="";
-          
-      // if 'n' is NOT evenly divisible by 2 (i.e.: n is odd)
-      if(n%2==1){
-         ans = "Weird";
-      }
-      else{
-         // Complete the code 
-      }
-      System.out.println(ans);
-   }
+
+	public static void main(String[] args) {
+
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();      
+		sc.close();
+
+		String ans="";
+		if(n%2==1) {
+			ans = "Weird";
+		} else {
+			if(2<=n && n<=5 || n>20){
+				ans = "Not Weird";
+			}else {
+				ans = "Weird";
+			}
+
+		}
+		System.out.println(ans);
+
+	}
 }
