341c6a63e06ff90818d421301f1074008eca4aed
Aravind1998 <aravindsuresh98@gmail.com>
2017-02-04 12:36:06 +0530

    Create Solution.java

---
+++ Day9/Solution.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+         static int Factorial(int n)
+            {
+            if(n==0)
+                 return 1;
+            else
+                 return (n*Factorial(n-1));
+        }
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        int fact = 1;
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+        fact = Factorial(number);
+       System.out.println(fact);
+    }
+    
+}
