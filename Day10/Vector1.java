a870dd723218ceaa4c039f184d4de7b2601a6fec
Meenakshi1998 <33223437+Meenakshi1998@users.noreply.github.com>
2018-10-07 10:51:44 +0530

    Create Vector1.java

---
+++ Day10/Vector1.java
@@ -0,0 +1,15 @@
+import java.util.Vector;
+class Vector1
+{
+  public static void main(String[] args)
+  {
+    Vector v = new Vector();
+
+    v.add(1);
+    v.add(2);
+    v.add("geeks");
+    v.add(3);
+
+    System.out.println("Vector is " + v);
+  }
+}
