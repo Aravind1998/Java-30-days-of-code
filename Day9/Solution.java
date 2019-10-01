682524fa8940b3f5f707c039f2542392c7b0d6a4
aravindsuresh98 <54808805+aravindsuresh98@users.noreply.github.com>
2019-10-01 09:37:06 +0530

    Update Solution.java

--- Day9/Solution.java
+++ Day9/Solution.java
@@ -10,7 +10,7 @@ public class Solution {
                  return (n*Factorial(n-1));
         }
     public static void main(String[] args) {
-        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        /* Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
         int fact = 1;
         Scanner sc = new Scanner(System.in);
         int number = sc.nextInt();
